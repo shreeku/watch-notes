eeba243f5729def42160d2b4ebb3b8150928d2d3
shreeku <shreeku009@gmail.com>
2023-12-10 20:56:31 -0500

    Initial Commit

---
+++ watch-notes Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  watch-notes Watch App
+//
+//  Created by Shree kumaar Rathinavelu sivasankaran on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watch-notes Watch App/watch_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch_notesApp.swift
+//  watch-notes Watch App
+//
+//  Created by Shree kumaar Rathinavelu sivasankaran on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct watch_notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
